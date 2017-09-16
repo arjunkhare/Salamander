792783af2de06c5381a7b7429ef0cbc8a4650a4e
Arjun Khare <akhare321@gmail.com>
2017-09-16 01:10:35 -0700

    lol

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-var audio = new Audio('ey-boss.wav');
-console.log("hello i am a boi")
-setInterval(
-		function() {
-			audio.play();
-		}, 5000);
+var eyboss = new Audio('ey-boss.wav');
+var cancer = new Audio('i-have-a-cancer.wav');
+var alien = new Audio('have-you-seen-alien.wav');
+var nyesss = new Audio('nyesssss.wav');
+
+var stop = new Audio('time-to-stop.wav');
+
+var playSound = function(){
+	// var val = Math.floor((Math.random() * 4) + 1);
+	// switch(val){
+	// 	case 1:
+	// 		eyboss.play();
+	// 		break;
+	// 	case 2:
+	// 		cancer.play();
+	// 		break;
+	// 	case 3:
+	// 		alien.play();
+	// 		break;
+	// 	case 4:
+	// 		nyesss.play();
+	// 		break;
+	// }
+}
+console.log("wat")
+setTimeout(function(){ playSound() }, 1000);
+// setTimeout(playSound, 3000);
+// setTimeout(playSound(), 5000);
+// setTimeout(playSound(), 7000);
+// setTimeout(playSound(), 9000);
+// setTimeout(stop.play(), 11000);
+
