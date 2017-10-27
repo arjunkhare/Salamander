c292805d0c50713b2b4542d6af4c773b064f62fa
Timothy-Toombs <timothytoombs@berkeley.edu>
2017-10-27 16:33:25 -0700

    Version 1.0 completed
    fully completed.

--- main.js
+++ main.js
@@ -2,28 +2,32 @@ var eyboss = new Audio('ey-boss.wav');
 var cancer = new Audio('i-have-a-cancer.wav');
 var alien = new Audio('have-you-seen-alien.wav');
 var nyesss = new Audio('nyesssss.wav');
-
+var startCount = 0
 var stop = new Audio('time-to-stop.wav');
 
 var playSound = function(){
-	// var val = Math.floor((Math.random() * 4) + 1);
-	// switch(val){
-	// 	case 1:
-	// 		eyboss.play();
-	// 		break;
-	// 	case 2:
-	// 		cancer.play();
-	// 		break;
-	// 	case 3:
-	// 		alien.play();
-	// 		break;
-	// 	case 4:
-	// 		nyesss.play();
-	// 		break;
-	// }
+	var val = Math.floor((Math.random() * 4) + 1);
+	startCount++;
+	if (startCount % 12 == 0) {
+		stop.play();
+	} else {
+		switch(val){
+			case 1:
+				eyboss.play();
+				break;
+			case 2:
+				cancer.play();
+				break;
+			case 3:
+				alien.play();
+				break;
+			case 4:
+				nyesss.play();
+				break;
+		}
+	}
 }
-console.log("wat")
-setTimeout(function(){ playSound() }, 1000);
+setInterval(function(){ playSound() }, 300000);
 // setTimeout(playSound, 3000);
 // setTimeout(playSound(), 5000);
 // setTimeout(playSound(), 7000);
